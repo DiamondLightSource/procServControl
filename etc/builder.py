5d79ffb02fd9f890796b15940bdb6f696304c709
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2011-08-24 16:14:23 +0000

    procServControl: initial checking ready for release

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,17 @@
+from iocbuilder import AutoSubstitution, Device
+from iocbuilder.modules.asyn import Asyn
+from iocbuilder.modules.seq import Seq
+from iocbuilder.modules.busy import Busy
 
+class procServControl(AutoSubstitution, Device):
+    '''Controls the procServ for an IOC'''
+    
+    Dependencies = (Asyn,Seq,Busy)
+    LibFileList = ["procServControl"]
+    DbdFileList = ["procServControl"]	
+    
+    ## Parse this template file for macros    
+    TemplateFile = 'procServControl.template'
 
+    def PostIocInitialise(self):
+        print 'seq(procServControl,"P=%(P)s")' % self.args
