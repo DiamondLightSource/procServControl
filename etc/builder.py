89fd2196ef92ddd9407b218840bc2d493d39088f
Nicoletta De Maio <Nicoletta.De-Maio@diamond.ac.uk>
2014-10-01 08:54:47 +0000

    removed Busy dependency from Builder support

--- etc/builder.py
+++ etc/builder.py
@@ -1,16 +1,15 @@
 from iocbuilder import AutoSubstitution, Device
 from iocbuilder.modules.asyn import Asyn
 from iocbuilder.modules.seq import Seq
-from iocbuilder.modules.busy import Busy
 
 class procServControl(AutoSubstitution, Device):
     '''Controls the procServ for an IOC'''
-    
-    Dependencies = (Asyn,Seq,Busy)
+
+    Dependencies = (Asyn,Seq)
     LibFileList = ["procServControl"]
-    DbdFileList = ["procServControl"]	
-    
-    ## Parse this template file for macros    
+    DbdFileList = ["procServControl"]
+
+    ## Parse this template file for macros
     TemplateFile = 'procServControl.template'
 
     def PostIocInitialise(self):
