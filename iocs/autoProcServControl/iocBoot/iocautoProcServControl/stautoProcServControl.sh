f0db9dde48a0fcd9940ea496b5462fbf6c6cfe26
Mateusz Hoppe <mateusz.hoppe@diamond.ac.uk>
2019-12-02 16:56:30 +0000

    Removed code duplication

--- iocs/autoProcServControl/iocBoot/iocautoProcServControl/stautoProcServControl.sh
+++ iocs/autoProcServControl/iocBoot/iocautoProcServControl/stautoProcServControl.sh
@@ -7,24 +7,35 @@ source $INIT_ROOT/functions
 SOFTIOCS=$(GetConfigDir $INIT_ROOT)/soft-iocs
 REALIOCS=$(GetConfigDir $INIT_ROOT)/real-iocs
 ST_FILE="/tmp/stautoProcServControl.cmd"
+ASYN_DB_FILE="/tmp/asyndb.cmd"
+SEQ_FILE="/tmp/seq.cmd"
 TOP=$(cd ../..; pwd)
 PROCSERVCONTROL=$(cd ../../../..; pwd)
 
 # Make a startup script, first the header
-cat <<EOF > $ST_FILE
+cat <<EOF > $ASYN_DB_FILE
 cd $TOP
 epicsEnvSet "EPICS_TS_MIN_WEST", '0'
 dbLoadDatabase "dbd/autoProcServControl.dbd"
 autoProcServControl_registerRecordDeviceDriver(pdbbase)
 EOF
 
-# Make asyn ports and database bits
+# IocInit
+cat <<EOF > $SEQ_FILE
+iocInit
+EOF
+
 sed '/^#/d;/^\s*$/d' "$SOFTIOCS" | (
     while read IOC PORT IOCARGS; do
+        # Make asyn ports and database bits
         if [ "$IOC" != "autoProcServControl" ]; then
-            cat <<EOF >> $ST_FILE
+            cat <<EOF >> $ASYN_DB_FILE
 drvAsynIPPortConfigure("${IOC}port", "localhost:${PORT}", 100, 0, 0)
 dbLoadRecords "${PROCSERVCONTROL}/db/procServControl.template", "P=${IOC},PORT=${IOC}port"
+EOF
+            # Sequencers
+            cat <<EOF >> $SEQ_FILE
+seq(procServControl,"P=${IOC}")
 EOF
         fi
     done
@@ -36,54 +47,27 @@ if [ -s "$REALIOCS" ]; then
         while read IOC HOST PORT IOCARGS; do
             # softioc argument in column 4 provided
             if [ "$IOCARGS" == "softioc" ]; then
-                cat <<EOF >> $ST_FILE
+                # Make asyn ports and database bits
+                cat <<EOF >> $ASYN_DB_FILE
 drvAsynIPPortConfigure("${IOC}port", "${HOST}:${PORT}", 100, 0, 0)
 dbLoadRecords "${PROCSERVCONTROL}/db/procServControl.template", "P=${IOC},PORT=${IOC}port"
+EOF
+                # Sequencers
+                cat <<EOF >> $SEQ_FILE
+seq(procServControl,"P=${IOC}")
 EOF
             # Or we look for a soft-iocs entry in the Windows host
             elif [ -s $INIT_ROOT/$HOST/soft-iocs ]; then
                 sed '/^#/d;/^\s*$/d' $INIT_ROOT/$HOST/soft-iocs | (
                     while read HOSTIOC HOSTPORT HOSTIOCARGS; do
                         if [ "${HOSTIOC,,}" == "${IOC,,}" -a "$PORT" == "$HOSTPORT" ]; then
-                            cat <<EOF >> $ST_FILE
+                            # Make asyn ports and database bits
+                            cat <<EOF >> $ASYN_DB_FILE
 drvAsynIPPortConfigure("${IOC}port", "${HOST}:${PORT}", 100, 0, 0)
 dbLoadRecords "${PROCSERVCONTROL}/db/procServControl.template", "P=${IOC},PORT=${IOC}port"
 EOF
-                        fi
-                    done
-                )
-            fi
-        done
-    )
-fi
-
-# IocInit
-echo iocInit >> $ST_FILE
-
-# Start sequencers
-# Make asyn ports and database bits
-sed '/^#/d;/^\s*$/d' "$SOFTIOCS" | (
-    while read IOC PORT IOCARGS; do
-        if [ "$IOC" != "autoProcServControl" ]; then
-            cat <<EOF >> $ST_FILE
-seq(procServControl,"P=${IOC}")
-EOF
-        fi
-    done
-)
-
-if [ -s "$REALIOCS" ]; then
-    sed '/^#/d;/^\s*$/d' "$REALIOCS" | (
-        while read IOC HOST PORT IOCARGS; do
-            if [ "$IOCARGS" == "softioc" ]; then
-                cat <<EOF >> $ST_FILE
-seq(procServControl,"P=${IOC}")
-EOF
-            elif [ -s $INIT_ROOT/$HOST/soft-iocs ]; then
-                sed '/^#/d;/^\s*$/d' $INIT_ROOT/$HOST/soft-iocs | (
-                    while read HOSTIOC HOSTPORT HOSTIOCARGS; do
-                        if [ "$HOSTIOC" == "$IOC" -a "$PORT" == "$HOSTPORT" ]; then
-                            cat <<EOF >> $ST_FILE
+                            # Sequencers
+                            cat <<EOF >> $SEQ_FILE
 seq(procServControl,"P=${IOC}")
 EOF
                         fi
@@ -93,6 +77,9 @@ EOF
         done
     )
 fi
+
+# merge asyn_db and seq files together and add to ST_FILE
+cat $ASYN_DB_FILE $SEQ_FILE > $ST_FILE
 
 # Now start the IOC
 exec ./autoProcServControl $ST_FILE
